3e54768de85e1b184c66b83e5c0b2f1f91a692f7
nayfazian <zainnf03@gmail.com>
2024-03-09 17:14:30 +0700

    update product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Product</h1>
+        <h1 class="mt-4">Product Naila</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item">Home</li>
             <li class="breadcrumb-item active">Product</li>
