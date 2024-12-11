4cb0df02f024eaeade789539849bc0770bc8ad1e
Devanhahaha <devanherdiansyah74@gmail.com>
2024-12-11 12:01:44 +0700

    Devan-PaketdataController.php

--- app/Http/Controllers/PaketdataController.php
+++ app/Http/Controllers/PaketdataController.php
@@ -11,117 +11,81 @@ use Illuminate\Support\Facades\Auth;
 
 class PaketdataController extends Controller
 {
+    const JENIS_PAKET = 'PAKETDATA';
+    const STATUS_LUNAS = 'lunas';
+    const PEMBAYARAN_CASH = 'cash';
 
-    protected $factory;
-    public function __construct() {
-        $this->factory = new PaketDataFactory();
-    }
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $data['paket']=Wallet::where('name', 'LIKE', '%Paketdata%')->get();
+        $data['paket'] = Wallet::where('name', 'LIKE', '%Paketdata%')->get();
         return view('paket_data', $data);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('addpaket_data');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        // Validasi request
+        $this->validateRequest($request);
+
+        $transaksi = $this->createTransaction($request);
+
+        $processedData = $this->processNominalAndCash($request->nominal);
+
+        $this->createPaketData($request, $transaksi, $processedData);
+
+        $this->kurangiStokPaketdata([
+            'nominal' => $processedData['nominal'],
+            'jenis' => $request->jenis,
+        ]);
+
+        return redirect()->route('laporanpaketdata.index');
+    }
+    private function validateRequest(Request $request)
+    {
         $request->validate([
             'name' => 'required|alpha',
             'number' => 'required|numeric',
             'jenis' => 'required|alpha',
             'nominal' => 'required',
         ]);
-        
-    
-    $jenis = 'PAKETDATA';
-    $prefix = "TRX-$jenis-";
-    $uniquePart = uniqid();
-    $code = strtoupper($prefix . substr($uniquePart, -6));
-
-    $data = explode(' - ',$request->nominal);
-    $cash =  preg_replace("/[^0-9]/", "", $data[1]);
-    
-
-    $transaksi = Transaksi::create([
-        'user_id' => Auth::user()->id,
-        'order_id' => $code,
-        'status' => 'lunas',
-        'jenis_transaksi' => $jenis,
-        'jenis_pembayaran' => 'cash'
-    ]);
-
-    $nominal = str_replace('.', '', $data[0]);
-
-    Paketdata::create([
-        'transaksi_id' => $transaksi->id,
-        'nama' => $request->name,
-        'no_telp' => $request->number,
-        'tipe_kartu' => $request->jenis,
-        'harga' => $cash,
-        'nominal' => $nominal,
-    ]);
-    
-    
-    
-    $this->kurangiStokPaketdata($nominal, $request->jenis);
-
-    return redirect()->route('laporanpaketdata.index');
-
-    
-}
-    private function kurangiStokPaketdata($nominal, $jenis) {
-        
-    $paketdata = Wallet::where('name', 'Paketdata '. $jenis ." ". $nominal)->first();
-
-    
-    if ($paketdata) {
-        $stokBaru = $paketdata->value - 1; 
-        $paketdata->update(['value' => $stokBaru]);
     }
-}
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    private function createTransaction()
     {
-        //
-    }
+        $prefix = "TRX-" . self::JENIS_PAKET . "-";
+        $uniquePart = uniqid();
+        $code = strtoupper($prefix . substr($uniquePart, -6));
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return Transaksi::create([
+            'user_id' => Auth::user()->id,
+            'order_id' => $code,
+            'status' => self::STATUS_LUNAS,
+            'jenis_transaksi' => self::JENIS_PAKET,
+            'jenis_pembayaran' => self::PEMBAYARAN_CASH,
+        ]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    private function processNominalAndCash($nominal)
     {
-        //
+        $data = explode(' - ', $nominal);
+        return [
+            'nominal' => str_replace('.', '', $data[0]),
+            'cash' => preg_replace('/\D/', '', $data[1]),
+        ];
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    private function createPaketData(Request $request, $transaksi, $processedData)
     {
-        //
+        Paketdata::create([
+            'transaksi_id' => $transaksi->id,
+            'nama' => $request->name,
+            'no_telp' => $request->number,
+            'tipe_kartu' => $request->jenis,
+            'harga' => $processedData['cash'],
+            'nominal' => $processedData['nominal'],
+        ]);
+    }
+    private function kurangiStokPaketdata($data)
+    {
+        $paketdata = Wallet::where('name', 'Paketdata ' . $data['jenis'] . " " . $data['nominal'])->first();
+
+        if ($paketdata) {
+            $stokBaru = $paketdata->value - 1;
+            $paketdata->update(['value' => $stokBaru]);
+        }
     }
 }
