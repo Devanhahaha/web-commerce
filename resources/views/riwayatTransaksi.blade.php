381014d9c88784d6b056407288d3e5d5faa0e37a
Devanhahaha <devanherdiansyah74@gmail.com>
2024-12-14 15:44:31 +0700

    naila-riwayattransaksi.blade

--- resources/views/riwayatTransaksi.blade.php
+++ resources/views/riwayatTransaksi.blade.php
@@ -1,35 +1,35 @@
 @extends('layout.customer.maincust')
 
 @section('content')
-<main>
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Riwayat Transaksi</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item">Home</li>
-            <li class="breadcrumb-item active">Riwayat Transaksi</li>
-        </ol>
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-table me-1"></i>
-                DataTable Example
-            </div>
-            <div class="card-body">
-                <table id="datatablesSimple">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>Nomor Telp</th>
-                            <th>Nominal</th>
-                            <th>Jenis Layanan</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                    </tfoot>
-                </table>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Riwayat Transaksi</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item active">Riwayat Transaksi</li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    DataTable Example
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Nomor Telp</th>
+                                <th>Nominal</th>
+                                <th>Jenis Layanan</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-</main>
-<main>
-@endsection
+    </main>
+    <main>
+    @endsection
