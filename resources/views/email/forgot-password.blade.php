13e9c74b756ae6527244527c924f07e216887782
Devanhahaha <devanherdiansyah74@gmail.com>
2024-12-14 12:48:33 +0700

    naila-forgot-password.blade

--- resources/views/email/forgot-password.blade.php
+++ resources/views/email/forgot-password.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Hallo {{ $nama }},</h1>
     <p>Di bawah ini kami cantumkan password baru anda</p>
     <p>Password Baru : @Commerce123</p>
 </body>
+
 </html>
