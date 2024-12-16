059cd924ffdb225f2b83d8760bca613c77794f09
Devanhahaha <devanherdiansyah74@gmail.com>
2024-12-16 11:47:01 +0700

    afrian-models-paketdata

--- app/Models/Paketdata.php
+++ app/Models/Paketdata.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PaketData extends Model implements FeatureInterface {
-    protected $table = 'paket_datas'; 
+class PaketData extends Model implements FeatureInterface
+{
+    protected $table = 'paket_datas';
     protected $guarded = [];
-
-    public function getType() {
+    public function getType()
+    {
         return 'Paket Data';
     }
 }
